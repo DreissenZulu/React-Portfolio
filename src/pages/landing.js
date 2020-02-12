028a6959cd57401a6c88d5a4c1a3fe39e7d62c1d
Alex Khoa Le <leduykhoa@hotmail.ca>
2020-02-12 12:18:39 -0500

    Overhauled landing page content. New cleaner minimal design

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,46 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from "react-router-dom";
-import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 function Landing() {
     return (
         <React.Fragment>
-            <div className="App-header">
-                <h4>I AM</h4>
-                <h1>Alex Khoa Le</h1>
-                <h4>FULL STACK WEB DEVELOPER</h4>
+            <div className="landing-text">
+                <h4 style={{lineHeight: "85%"}}>Hey there! I'm</h4>
+                <h1>ALEX KHOA LE</h1>
+                <h4>Full Stack Web Developer</h4>
+                <Link to="/about">
+                    <Button variant="primary" className="landing-button">Enter Site</Button>
+                </Link>
             </div>
-            <Container className="landing-container d-flex align-items-stretch">
-                <Row className="flex-fill">
-                    <Col md={6} className="landing-spacing">
-                        <Link to="/about" className="landing-button landing-about">
-                            <div className="about-btn">
-                                <p className="landing-btn-text">About Me</p>
-                            </div>
-                        </Link>
-                    </Col>
-                    <Col md={6} className="d-flex align-items-stretch">
-                        <Row className="flex-fill">
-                            <Col md={12} sm={6} xs={12} className="landing-spacing">
-                                <Link to="/portfolio" className="landing-button">
-                                <div className="portfolio-btn">
-                                    <p className="landing-btn-text">Portfolio</p>
-                                </div>
-                                </Link>
-                            </Col>
-                            <Col md={12} sm={6} xs={12} className="landing-spacing">
-                                <Link to="/contact" className="landing-button">
-                                    <div className="contact-btn">
-                                        <p className="landing-btn-text">Contact</p>
-                                    </div>
-                                </Link>
-                            </Col>
-                        </Row>
-                    </Col>
-                </Row>
-            </Container>
+            <Row className="landing-bg">
+                <Col lg={10} md={12}className="landing-blue">
+                </Col>
+                <Col lg={2} className="landing-img"></Col>
+            </Row>
+            
         </React.Fragment>
     )
 }
